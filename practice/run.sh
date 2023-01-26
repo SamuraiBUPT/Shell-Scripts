0f124c59a9fe50147f8d40f10b03ff49689322db
SamuraiBUPT <2781374337@qq.com>
2023-01-27 02:04:09 +0800

    change back(√)

--- practice/run.sh
+++ practice/run.sh
@@ -4,6 +4,6 @@ echo "Shell Script 【run.sh】 starts working..."
 echo "written by Samurai_BUPT"
 
 min_RAM=2
-max_RAM=6
+max_RAM=8
 
 java -server -Xms${min_RAM}G -Xmx${max_RAM}G -jar -XX:+UseG1GC mohist-1.16.5-1096-server.jar --nogui
