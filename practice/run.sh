2c20b5944f1949b19391d406dc9ca10ce09a3f0c
SamuraiBUPT <2781374337@qq.com>
2023-01-27 10:24:11 +0800

    tidy jobs

--- practice/run.sh
+++ practice/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Shell Script 【run.sh】 starts working..."
-echo "written by Samurai_BUPT"
+echo " _____                                              _ "
+echo "/  ___|                                            (_)"
+echo "\ \`--.    __ _   _ __ ___    _   _   _ __    __ _   _ "
+echo " \`--. \  / _\` | | '_ \` _ \  | | | | | '__|  / _\` | | |"
+echo "/\__/ / | (_| | | | | | | | | |_| | | |    | (_| | | |"
+echo "\____/   \__,_| |_| |_| |_|  \__,_| |_|     \__,_| |_|"
 
 min_RAM=2
 max_RAM=8
--- tool/print_name/print_.sh
+++ tool/print_name/print_.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 printed_string="Samurai"
-font="doom"
+font="univers"
 
 bash ./Ascii_Signature/ascii_signature.sh -s $printed_string -f $font
