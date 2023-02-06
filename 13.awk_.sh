5b92602d7fc6d4150fd2f22c71e0aa84f21fb949
SamuraiBUPT <2781374337@qq.com>
2023-02-06 11:50:35 +0800

    add tutorial.sh

--- 13.awk_.sh
+++ 13.awk_.sh
@@ -13,3 +13,7 @@ cat Test | awk '{print $1}'
 # 所以下次就不用去变换数组了，直接用awk即可
 var_str="I test your code"
 echo $var_str | awk '{print $2}'
+
+# 这里可以认识到：单引号里面的是一个命令环境，可以执行命令，split是awk自带的一个函数，$1的意思是第一列，以.作为分隔符，结果存储到arr里面，这个数组的下标也是从1开始的
+first=$(python -V 2>&1 | awk '{print $2}' | awk '{split($1, arr, "."); print arr[1]}')
+second=$(python -V 2>&1 | awk '{print $2}' | awk '{split($1, arr, "."); print arr[2]}')
---
+++ python_tutorial.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "This is a tutorial for server to install python packages."
+echo ""
+echo "Please first check your 'apt', 'apt-get', 'wget'...commandLine is working or not."
+echo ""
+echo "It' s recommended to update/upgrade first and then follow these steps."
+echo ""
+echo "First: Conda."
+
+first=$(python -V 2>&1 | awk '{print $2}' | awk '{split($1, arr, "."); print arr[1]}')
+second=$(python -V 2>&1 | awk '{print $2}' | awk '{split($1, arr, "."); print arr[2]}')
+if [[ $first == "3" ]]; then
+    if [[ $second -ge "7" ]]; then
+        echo "python version check pass."
+    else
+        echo "your python version is too low, please upgrade."
+        exit
+    fi
+fi
+
+echo "install conda"
+echo "Please mkdir a folder named 'Download'."
+echo "and cd there"
+echo ""
+echo "input ' wget https://mirrors.bfsu.edu.cn/anaconda/archive/Anaconda3-2021.11-Linux-x86_64.sh ' to download."
+echo ""
+echo "input ' bash Anaconda3-2021.11-Linux-x86_64.sh ' to install"
+echo ""
+echo "remember to change install location to /anaconda3 (home path)"
+echo "then re-connect your server to update."
+
+echo ""
+echo "use ' conda create --name pytorch python=your_version ' to create a envs"
+echo "follow the tips"
+
+echo ""
+echo ""
+echo ""
+echo "input : 'conda install pytorch torchvision torchaudio cpuonly -c pytorch' "
